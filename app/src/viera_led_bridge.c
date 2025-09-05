2849d429fe61a7dc958fbe0e07987ca5e6075ab9
nicho1asdev <50599999+nicho1asdev@users.noreply.github.com>
2025-09-05 20:51:53 +0200

    Update viera_led_bridge.c

--- app/src/viera_led_bridge.c
+++ app/src/viera_led_bridge.c
@@ -1,17 +1,12 @@
-#include "viera_led_state.h"
-void viera_on_brightness_changed(uint8_t level) {
-    if (level > 100) level = 100;
-    viera_user_brightness_set(level);
-#if IS_ENABLED(CONFIG_ZMK_RGB_UNDERGLOW)
-    zmk_rgb_underglow_set_brt(level);
-    zmk_rgb_underglow_resume();
-#endif
-}
 // SPDX-License-Identifier: MIT
 #include <zephyr/logging/log.h>
+#include <zephyr/sys/atomic.h>
 LOG_MODULE_REGISTER(viera_led_bridge, CONFIG_LOG_DEFAULT_LEVEL);
 
-#include "viera_led_state.h"
+/* ---- Master user brightness state (0..100) ---- */
+static atomic_t g_user_brt = ATOMIC_INIT(80);
+uint8_t viera_user_brightness_get(void) { return (uint8_t)atomic_get(&g_user_brt); }
+void    viera_user_brightness_set(uint8_t v) { if (v > 100) v = 100; atomic_set(&g_user_brt, v); }
 
 /* Detect if ZMK underglow public API header is available at build time */
 #if __has_include(<zmk/rgb_underglow.h>)
@@ -24,13 +19,10 @@ LOG_MODULE_REGISTER(viera_led_bridge, CONFIG_LOG_DEFAULT_LEVEL);
 void viera_on_brightness_changed(uint8_t level) {
     if (level > 100) level = 100;
     viera_user_brightness_set(level);
-
 #if VIERA_HAS_UNDERGLOW
-    /* If underglow API is present in this ZMK build, update it as well */
     zmk_rgb_underglow_set_brt(level);
     zmk_rgb_underglow_resume();
 #else
-    /* No underglow API available; effects should read viera_user_brightness_get() */
     ARG_UNUSED(level);
     LOG_DBG("Underglow API not present; using master brightness only");
 #endif
